2cbdd129242c8b3bf6a435ecea9d46f467e6c7d4
Bruno Trevizani <brunotrevizani@ienh.com.br>
2026-04-29 00:32:58 -0300

    correções finais para funcionalidade do railways

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "dev",
-  password: "1234",
-  database: "barbearia",
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
-    console.error("Erro ao conectar:", err);
+    console.error("Erro ao conectar no MySQL:", err);
   } else {
-    console.log("MySQL conectado");
+    console.log("MySQL conectado ✅");
+    connection.release();
   }
 });
 
-module.exports = connection;
+module.exports = pool;
