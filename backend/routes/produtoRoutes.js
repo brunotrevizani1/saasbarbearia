29d7952853a8a1bcb605996c667a85cfac8aede2
Bruno Trevizani <brunotrevizani@ienh.com.br>
2026-05-02 00:26:00 -0300

    corrige import listar produtos cliente

--- backend/routes/produtoRoutes.js
+++ backend/routes/produtoRoutes.js
@@ -4,14 +4,15 @@ const router = express.Router();
 const {
   criarProduto,
   listarProdutos,
+  listarProdutosCliente,
   deletarProduto,
   reservarProduto,
 } = require("../controllers/produtoController");
 
 router.post("/produtos", criarProduto);
 router.get("/produtos", listarProdutos);
+router.get("/produtos/cliente", listarProdutosCliente);
 router.delete("/produtos/:id", deletarProduto);
 router.post("/produtos/reservar", reservarProduto);
-router.get("/", listarProdutosCliente);
 
 module.exports = router;
